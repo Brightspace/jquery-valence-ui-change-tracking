6f0b645303d659d0f12ab76c644472ff4985df6f
Dave Batiste <dave.batiste@Desire2Learn.com>
2014-01-14 12:27:40 -0500

    Adding changeTracker and basic tests to go with.

---
+++ changeTracker.js
@@ -0,0 +1,74 @@
+﻿/*jslint browser: true*/
+
+( function( vui ) {
+
+	'use strict';
+
+	// Check if the provided vui global is defined, otherwise try to require it if
+	// we're in a CommonJS environment; otherwise we'll just fail out
+	if( vui === undefined ) {
+		if( typeof require === 'function' ) {
+			vui = require('../../core');
+		} else {
+			throw new Error('load vui first');
+		}
+	}
+
+	// Export the vui object if we're in a CommonJS environment.
+	// It will already be on the window otherwise
+	if( typeof module === 'object' && typeof module.exports === 'object' ) {
+		module.exports = vui;
+	}
+
+	var $ = vui.$;
+
+	$.widget( 'vui.vui_changeTracker', {
+
+		_create: function() {
+
+			var $node = $( this.element );
+
+			var changedItems = {};
+
+			$node.on( 'vui-changed', function( e, args ) {
+
+				changedItems[args.id] = true;
+
+				$node.addClass( 'vui-changed' );
+
+			} ).on( 'vui-restored', function( e, args ) {
+
+				if ( changedItems[args.id] !== undefined ) {
+					delete changedItems[args.id];
+				}
+
+				if ( Object.keys( changedItems ).length === 0 ) {
+					$node.removeClass( 'vui-changed' );
+				} 
+
+			} );
+
+		},
+
+		_destroy: function () {
+
+			$( this.element )
+				.removeClass( 'vui-changed' )
+				.off( 'vui-changed vui-restored' );
+
+		},
+
+		hasChanged: function() {
+			return $( this.element ).hasClass( 'vui-changed' );
+		}
+
+	} );
+
+	vui.addClassInitializer(
+			'vui-field-row',
+			function( node ) {
+				$( node ).vui_changeTracker();
+			}
+		);
+
+} )( window.vui );
