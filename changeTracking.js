44b2952027947c0a6567df14fb877b60b34b6881
Dave Batiste <dave.batiste@Desire2Learn.com>
2014-01-15 15:25:03 -0500

    Adding support for radio buttons for change tracking.

--- changeTracking.js
+++ changeTracking.js
@@ -27,13 +27,13 @@
 		_create: function() {
 
 			var $node = $( this.element );
+			var nodeName = $node.prop( 'nodeName' );
 
 			$node.uniqueId();
 
 			var getValue = function( $tempNode ) {
 
 				var node = $tempNode.get(0);
-				var nodeName = node.nodeName;
 
 				if ( nodeName === 'SELECT' ) {
 					return $tempNode.val();
@@ -53,14 +53,11 @@
 
 			};
 
-			var originalValue = getValue( $node );
+			var triggerEvent = function( $target ) {
 
-			$node.on( 'change.vui', function( e ) {
-
-				var $target = $( e.target );
 				var args = { 'id': $target.attr( 'id' ) };
 
-				if ( getValue( $target ) === originalValue ) {
+				if ( getValue( $target ) === $target.data( 'originalValue' ) ) {
 
 					$target
 						.removeClass( 'vui-changed' )
@@ -73,6 +70,45 @@
 						.trigger( 'vui-change', args );
 
 				}
+
+			};
+
+			if ( nodeName === 'INPUT' && $node.attr( 'type' ) === 'radio' ) {
+				var groupName = $node.prop( 'name' );
+				if ( groupName ) {
+					var selectedValue = $( 'input[name="' + groupName + '"]:checked' ).val();
+					$node.data( 'selectedValue', selectedValue );
+				}
+			}
+
+			$node.data( 'originalValue', getValue( $node ) );
+
+			$node.on( 'change.vui', function( e ) {
+
+				var $target = $( e.target );
+
+				if ( nodeName === 'INPUT' && $node.attr( 'type' ) === 'radio' && $node.prop( 'name' ) ) {
+
+					$( 'input[name="' + groupName + '"]' ).each( function( i ) {
+
+						if ( this !== e.target ) {
+
+							var $this = $( this );
+
+							if ( $this.val() === $target.data( 'selectedValue' ) ) {
+								triggerEvent( $this );
+							}
+
+							$this.data( 'selectedValue', $target.val() );
+						}
+
+					} );
+
+					$target.data( 'selectedValue', $target.val() );
+
+				}
+
+				triggerEvent( $target );
 
 			} );
 
